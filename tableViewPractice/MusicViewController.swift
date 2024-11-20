8d3dc0a79fe74a0d1e4396e5cd86139a5b175fd9
DharmaMithra <dharmarockz2001@gmail.com>
2024-11-19 18:21:40 -0600

    now song will be played if the user selects any song

--- tableViewPractice/MusicViewController.swift
+++ tableViewPractice/MusicViewController.swift
@@ -4,23 +4,30 @@
 //
 //  Created by Srikanth Kyatham on 11/18/24.
 //
-
+import AVFoundation
 import UIKit
 
 class MusicViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var audioPlayer : AVAudioPlayer?
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var nowPlayingView: UIView!
+    @IBOutlet weak var nowPlayingImageView: UIImageView!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var nowPlayingArtistLabel: UILabel!
+    @IBOutlet weak var nowPlayingTitleLabel: UILabel!
     
     let songs = [
-            ("The Kids Aren't Alright", "Fall Out Boy - Single", "song1"),
-            ("Sorry", "Justin Bieber - Single", "song2"),
-            ("Turn Your Love", "Half Moon Run - Single", "song3"),
-            ("Blinding Lights", "The Weeknd - Single", "song4"),
-            ("Shape of You", "Ed Sheeran - Single", "song5"),
-            ("Levitating", "Dua Lipa - Future Nostalgia", "song6"),
-            ("Someone Like You", "Adele - 21", "song7"),
-            ("Rolling in the Deep", "Adele - 21", "song8"),
-            ("Circles", "Post Malone - Hollywood's Bleeding", "song1"),
-            ("Bohemian Rhapsody", "Queen - A Night at the Opera", "song2")
+        ("The Kids Aren't Alright", "Fall Out Boy - Single", "song1", "song1"),
+        ("Sorry", "Justin Bieber - Single", "song2","song1"),
+        ("Turn Your Love", "Half Moon Run - Single", "song3","song3"),
+        ("Blinding Lights", "The Weeknd - Single", "song4", "song4"),
+        ("Shape of You", "Ed Sheeran - Single", "song5","song5"),
+        ("Levitating", "Dua Lipa - Future Nostalgia", "song6","song6"),
+        ("Someone Like You", "Adele - 21", "song7","song7"),
+        ("Rolling in the Deep", "Adele - 21", "song8","song8"),
+        ("Circles", "Post Malone - Hollywood's Bleeding", "song1", "song1"),
+        ("Bohemian Rhapsody", "Queen - A Night at the Opera", "song2","song2")
         ]
     
     override func viewDidLoad() {
@@ -42,6 +49,24 @@ class MusicViewController: UIViewController, UITableViewDataSource, UITableViewD
         cell.configure(with: song.0, subtitle: song.1, imageName: song.2)
         
         return cell
+    }
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let song = songs[indexPath.row]
+        playSong(named : song.3)
+    }
+    func playSong(named fileName: String) {
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: "mp3") else {
+                print("Error: File \(fileName).mp3 not found")
+                return
+            }
+
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: url)
+                audioPlayer?.play()
+                print("Playing \(fileName)")
+            } catch {
+                print("Error playing \(fileName): \(error.localizedDescription)")
+            }
     }
 
 }
